16af08ba723b5085d89de4511627ff2aaaaec9cb
Douae <douaeelyaaqoubi2@gmail.com>
2026-01-20 02:10:57 +0100

    ADD: About section without carousel

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,22 @@ dropdowns.forEach(drop => {
         menu.style.display = drop.classList.contains('active') ? 'block' : 'none';
     });
 });
+
+/* ABOUT US*/
+const playBtn = document.querySelector(".play-btn");
+const videoImages = document.querySelectorAll(".video-box .video-img");
+
+let videoIndex = 0;
+
+playBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+
+    videoImages[videoIndex].classList.remove("active");
+
+    videoIndex++;
+    if (videoIndex >= videoImages.length) {
+        videoIndex = 0;
+    }
+
+    videoImages[videoIndex].classList.add("active");
+});
